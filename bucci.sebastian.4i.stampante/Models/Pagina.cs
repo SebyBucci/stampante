c32afcece6a4a721d0809281ca2018402bae8a17
Sebastian Bucci <127589946+SebyBucci@users.noreply.github.com>
2023-12-06 08:11:37 +0100

    Add files via upload

---
+++ bucci.sebastian.4i.stampante/Models/Pagina.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bucci.sebastian._4i.stampante.Models
+{
+    public class Pagina
+    {
+        public int C { get; set; }
+        public int M { get; set; }
+        public int Y { get; set; }
+        public int B { get; set; }
+    }
+}
---
+++ bucci.sebastian.4i.stampante/Models/stampante.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace bucci.sebastian._4i.stampante.Models
+{
+    public class stampante
+    {
+        public int C { get; set; }
+        public int M { get; set; }
+        public int Y { get; set; }
+        public int B { get; set; }
+
+        // - ... e un cassetto di fogli(tutte property int)
+        public int Fogli { get; set; }
+        public stampante()
+        {
+            C = M = Y = B = 100;
+            Fogli = 200;
+        }
+        public bool Stampa (Pagina p)
+        {
+            if (Fogli > 0 
+                && C >= p.C
+                && M >= p.M
+                && Y >= p.Y
+                && B >= p.B)
+            {
+ 
+                C -= p.C;
+                M -= p.M;
+                Y -= p.Y;
+                B -= p.B;
+                Fogli--;
+
+                return true;
+                }
+            return false;
+            }
+        // - torna la quantità di inchiostro presente nei 4 serbatoi.
+        public int StatoInchiostro(string colore )
+        {
+            
+        }
+
+        }
+    }
+
